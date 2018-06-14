b62e0b2023f05c6bad48fcf903bd265619f86aed
linux-downey <1148256554@qq.com>
2018-06-14 12:26:27 +0800

    source file

---
+++ Seeed_CY8C401XX.h
@@ -0,0 +1,77 @@
+/*
+ * Seeed_CY8C401XX.h
+ * Driver for DIGITAL I2C HUMIDITY AND TEMPERATURE SENSOR
+ *  
+ * Copyright (c) 2018 Seeed Technology Co., Ltd.
+ * Website    : www.seeed.cc
+ * Author     : downey
+ * Create Time: June 2018
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _SEEED_CY8C401XX_H
+#define _SEEED_CY8C401XX_H
+
+
+#include <Wire.h>
+#include <Arduino.h>
+
+typedef int s32;
+typedef long unsigned int u32;
+typedef unsigned char u8;
+typedef unsigned short u16;
+
+/*default iic addr is 0x08*/
+#define DEFAULT_IIC_ADDR  0x08
+
+#define TOUCH_BOTTON_VALUE_REG_ADDR  0X00
+#define TOUCH_SLIDER_VALUE_REG_ADDR  0X01
+
+
+class IIC_OPRTS
+{
+    public:
+        void IIC_begin(){Wire.begin();}
+        s32 IIC_write_byte(u8 reg,u8 byte);
+        void IIC_read_byte(u8 reg,u8* byte);
+        void set_iic_addr(u8 IIC_ADDR);
+        void IIC_read_16bit(u8 start_reg,u16 *value);
+        s32 IIC_write_16bit(u8 reg,u16 value);
+    private:
+        u8 _IIC_ADDR;
+};
+
+class CY8C:public IIC_OPRTS
+{
+    public:
+        CY8C(u8 addr=DEFAULT_IIC_ADDR);
+        void init();
+        s32 get_touch_botton_value(u8* touch_value);
+        s32 get_touch_slider_value(u8* value);
+    private:
+        u8 _IIC_ADDR;
+};
+
+
+#endif
+
